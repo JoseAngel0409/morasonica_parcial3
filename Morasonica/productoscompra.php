16cd7f27509b4b3053f33448c658d9479a6b4649
franciscolopez22 <99852344+franciscolopez22@users.noreply.github.com>
2022-12-01 20:33:03 -0600

    Avance de pagina de cada producto

---
+++ Morasonica/productoscompra.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MoraSonica</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <br>
+    <div class="container">
+        <div class="row"><br><br>
+            <div class="col-12">
+                <div class="jumbotron">
+                    <div class="card-deck">
+                        <div class="card mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="producto.png" width="200" height="170">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Producto</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <a href=""><button type="submit" class="btn btn-outline-light">Comprar</button></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center">
+        <hr>
+        2022 &copy; Cetis107 Desarrollo Web
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
